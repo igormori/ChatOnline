04fb7c8ce63adeb632c25b56aa06b727ed345f03
igorMori <igor_canada@outlook.com>
2019-04-20 00:24:58 -0400

    s
    s

--- backend/server.js
+++ backend/server.js
@@ -4,6 +4,7 @@ var path = require('path');
 var app = express();
 var server = http.Server(app);
 var cors = require('cors')
+const PORT = process.env.PORT || 3030
 
 //mangoose connection
 var db = require('./dbConnection.js')
@@ -11,6 +12,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors()) // Use this after the variable declaration
+
 
 //socke.io
 var socketIO = require('socket.io');
@@ -21,6 +23,8 @@ let routes = require("./routes/routes.js");
 
 //routes to use
 app.use('/api',routes); 
+
+app.use(express.static(path.join(__dirname,'../frontend/onlinechat')))
 
 app.set('port', 5000);
 
