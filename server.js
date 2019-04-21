ece3130ecb1376e9687a07379172397b2f9b3e80
igorMori <igor_canada@outlook.com>
2019-04-20 21:23:21 -0400

    s
    s

--- server.js
+++ server.js
@@ -12,7 +12,7 @@ const bodyParser = require('body-parser');
 app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: true }));
 app.use(cors()) // Use this after the variable declaration
-app.use(express.static(path.join(__dirname,'./frontend/onlinechat/build')))
+
 
 //socke.io
 var socketIO = require('socket.io');
@@ -23,6 +23,7 @@ let routes = require("./backend/routes/routes.js");
 
 //routes to use
 app.use('/api',routes); 
+app.use(express.static(path.join(__dirname,'./frontend/onlinechat/build')))
 
 app.get('*',(req,res) =>{
   res.sendFile(path.join(__dirname + './frontend/onlinechat/build/index.html'))
