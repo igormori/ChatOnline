d8823c060f134f4d9a635ed71ce65d472ab8eebc
igorMori <igor_canada@outlook.com>
2019-04-21 11:08:14 -0400

    Update navigation.js
    update navigation

--- frontend/onlinechat/src/navigation/navigation.js
+++ frontend/onlinechat/src/navigation/navigation.js
@@ -29,7 +29,7 @@ function ButtonAppBar(props) {
 
  let handleLogout = event => {
     localStorage.removeItem('token')
-    window.location = '/chat'
+    window.location = '/'
   };
 
     if(isLoggedIn){
